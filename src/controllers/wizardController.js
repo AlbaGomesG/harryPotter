ca49d95ac07982cb6366a24364e38f17549e0d42
AlbaGomesG <162628812+AlbaGomesG@users.noreply.github.com>
2025-04-08 11:35:37 -0300

    adicionando filtro por nome em wizards

--- src/controllers/wizardController.js
+++ src/controllers/wizardController.js
@@ -1,9 +1,9 @@
-const { createHouse, updateHouse } = require("../models/houseModel");
 const wizardModel = require("../models/wizardModel");
 
 const getAllWizards = async (req, res) => {
     try {
-        const wizards = await wizardModel.getWizards();
+        const { name } = req.query;
+        const wizards = await wizardModel.getWizards(name);
         res.json(wizards);
     } catch (error) {
         res.status(500).json({ message: "Erro ao buscar bruxos." });
